3aa6b9f6f953c56fac860d11f21c1b29d6ebf608
Thulani <nalebaniokiy99@gmail.com>
2022-07-16 06:33:15 -0700

    sorting_algorithms

--- sort.h
+++ sort.h
@@ -27,6 +27,15 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+void sort_deck(deck_node_t **deck);
+void quick_sort_hoare(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
+void counting_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void shell_sort(int *array, size_t size);
 
 /*Aux functions*/
 int partition(int *array, int low, int high,  size_t size);
